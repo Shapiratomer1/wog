ac8f07763e3151b59c1f4f819e0467db301bcc09
Tomer <tomer.shapira@autobrains.ai>
2024-09-02 19:16:12 +0300

    fix code from remarks

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-
 def welcome():
     username = input("Please enter your name: ")
     print(f'Hi {username} and welcome to the World of Games: The Epic Journey!')
 
 
-def start_play():
+def validate_user_input(user_choice: int, valid_inputs: range) -> bool:
+    if user_choice not in valid_inputs:
+        print(f'Please select a valid input using numbers {valid_inputs[0]}-{valid_inputs[-1]}')
+        return False
+    return True
 
-    def _validate_user_input(user_choice: str, valid_inputs: []) -> bool:
-        if user_choice not in valid_inputs:
-            print(f'Please select a valid input using numbers {valid_inputs[0]}-{valid_inputs[-1]}')
-            return False
-        return True
 
-    def _prompt_game_choice() -> str:
+def prompt_game_choice() -> int:
+    while True:
         user_game_choice = input("""
         Please choose a game to play:
         1. Memory Game - a sequence of numbers will appear for 1 second, and you have to guess it back.
@@ -21,27 +20,28 @@ def start_play():
         (to exit enter 'q')
         """)
         if user_game_choice == 'q':
-            exit(0)
+            print("Exiting the game. Goodbye!")
+            return -1  # Return a special value to indicate exit
 
-        is_game_input_valid = _validate_user_input(user_game_choice, ['1', '2', '3'])
-        if is_game_input_valid:
-            return user_game_choice
-        else:
-            _prompt_game_choice()
+        if validate_user_input(int(user_game_choice), range(1, 4)):
+            return int(user_game_choice)
 
-    def _prompt_difficulty_choice() -> str:
+
+def prompt_difficulty_choice() -> int:
+    while True:
         user_difficulty_choice = input("Please choose your game difficulty 1-5 (to exit enter 'q'): ")
         if user_difficulty_choice == 'q':
-            exit(0)
-
-        is_difficulty_input_valid = _validate_user_input(user_difficulty_choice, ['1', '2', '3', '4', '5'])
-        if is_difficulty_input_valid:
-            return user_difficulty_choice
-        else:
-            _prompt_difficulty_choice()
-
-    game_choice = _prompt_game_choice()
-    difficulty_choice = _prompt_difficulty_choice()
+            print("Exiting the game. Goodbye!")
+            return -1
+        if validate_user_input(int(user_difficulty_choice), range(1, 6)):
+            return int(user_difficulty_choice)
 
 
-
+def start_play():
+    game_choice = prompt_game_choice()
+    if game_choice == -1:
+        return
+    difficulty_choice = prompt_difficulty_choice()
+    if difficulty_choice == -1:
+        return
+    print(f"You have chosen game {game_choice} with difficulty level {difficulty_choice}.")
